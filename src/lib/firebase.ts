70d03ed1154d59899b458e81ec0d0f0bb4863cd1
bmbgsaubc <bmbgsa.ubc@gmail.com>
2026-03-09 17:08:48 +0000

    added firebase project to firebase ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,13 +3,14 @@ import { getAuth, signInAnonymously } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getFunctions } from "firebase/functions";
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "YOUR_KEY",
-  authDomain: "AxisWars.firebaseapp.com",
+  apiKey: "AIzaSyBO2MTjG8HZQEbtpqpQSIqWYFof3QH1j8E",
+  authDomain: "axiswars.firebaseapp.com",
   projectId: "axiswars",
-  storageBucket: "AxisWars.appspot.com",
+  storageBucket: "axiswars.firebasestorage.app",
   messagingSenderId: "187923812440",
-  appId: "YOUR_APP_ID",
+  appId: "1:187923812440:web:2a9c1ed299cf6081bdba2d"
 };
 
 export const app = initializeApp(firebaseConfig);
